370ae03a230845b496d447d81bc68579f6323d98
Jose Luis <joseluis.elizondof11@outlook.com>
2022-08-28 13:33:26 -0500

    Added bars for stats

--- components/PokemonStats.jsx
+++ components/PokemonStats.jsx
@@ -1,35 +1,53 @@
 function PokemonStats(props){
+    const hp_bar= ((100*props.stats[0].base_stat)/250)+"px"
+    const att_bar= ((100*props.stats[1].base_stat)/250)+"px"
+    const def_bar= ((100*props.stats[2].base_stat)/250)+"px"
+    const spa_bar= ((100*props.stats[3].base_stat)/250)+"px"
+    const spd_bar= ((100*props.stats[4].base_stat)/250)+"px"
+    const spe_bar= ((100*props.stats[5].base_stat)/250)+"px"
 
     return(
         <div className="pokemonStats">
             <div className="pokemonStats--stat-div">
                 <h2 className="pokemonStats--stat-div--stats">
-                    <span>HP:</span> {props.stats[0].base_stat}
+                    <span>HP:</span> 
+                    <div className="pokemonStats--stat-div--stats--stat-bar" style={{width: hp_bar}}/>
+                    {props.stats[0].base_stat}
                 </h2>
             </div>
             <div className="pokemonStats--stat-div">
                 <h2 className="pokemonStats--stat-div--stats">
-                    <span>Attack:</span> {props.stats[1].base_stat}
+                    <span>Attack:</span> 
+                    <div className="pokemonStats--stat-div--stats--stat-bar" style={{width: att_bar}}/>
+                    {props.stats[1].base_stat}
                 </h2>
             </div>
             <div className="pokemonStats--stat-div">
                 <h2 className="pokemonStats--stat-div--stats">
-                    <span>Defense:</span> {props.stats[2].base_stat}
+                    <span>Defense:</span>
+                    <div className="pokemonStats--stat-div--stats--stat-bar" style={{width: def_bar}}/>
+                    {props.stats[2].base_stat}
                 </h2>
             </div>
             <div className="pokemonStats--stat-div">
                 <h2 className="pokemonStats--stat-div--stats">
-                    <span>Sp. Atk:</span> {props.stats[3].base_stat}
-                </h2>
-            </div>
-            <div className="pokemonStats--stat-div">
-                <h2 className="pokemonStats--stats">
-                    <span>Sp. Def:</span> {props.stats[4].base_stat}
+                    <span>Sp. Atk:</span>
+                    <div className="pokemonStats--stat-div--stats--stat-bar" style={{width: spa_bar}}/>
+                    {props.stats[3].base_stat}
                 </h2>
             </div>
             <div className="pokemonStats--stat-div">
                 <h2 className="pokemonStats--stat-div--stats">
-                    <span>Speed:</span> {props.stats[5].base_stat}
+                    <span>Sp. Def:</span>
+                    <div className="pokemonStats--stat-div--stats--stat-bar" style={{width: spd_bar}}/>
+                    {props.stats[4].base_stat}
+                </h2>
+            </div>
+            <div className="pokemonStats--stat-div">
+                <h2 className="pokemonStats--stat-div--stats">
+                    <span>Speed:</span>
+                    <div className="pokemonStats--stat-div--stats--stat-bar" style={{width: spe_bar}}/>
+                    {props.stats[5].base_stat}
                 </h2>
             </div>    
         </div>
