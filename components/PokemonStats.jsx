bacd4055bf1ea5889d9c99d4062ef0136e80f0cb
Jose Luis <joseluis.elizondof11@outlook.com>
2022-08-28 12:36:47 -0500

    trying to add animations

--- components/PokemonStats.jsx
+++ components/PokemonStats.jsx
@@ -2,24 +2,36 @@ function PokemonStats(props){
 
     return(
         <div className="pokemonStats">
-            <h2 className="pokemonStats--stats">
-                <span>HP:</span> {props.stats[0].base_stat}
-            </h2>
-            <h2 className="pokemonStats--stats">
-                <span>Attack:</span> {props.stats[1].base_stat}
-            </h2>
-            <h2 className="pokemonStats--stats">
-                <span>Defense:</span> {props.stats[2].base_stat}
-            </h2>
-            <h2 className="pokemonStats--stats">
-                <span>Sp. Atk:</span> {props.stats[3].base_stat}
-            </h2>
-            <h2 className="pokemonStats--stats">
-                <span>Sp. Def:</span> {props.stats[4].base_stat}
-            </h2>
-            <h2 className="pokemonStats--stats">
-                <span>Speed:</span> {props.stats[5].base_stat}
-            </h2>
+            <div>
+                <h2 className="pokemonStats--stats">
+                    <span>HP:</span> {props.stats[0].base_stat}
+                </h2>
+            </div>
+            <div>
+                <h2 className="pokemonStats--stats">
+                    <span>Attack:</span> {props.stats[1].base_stat}
+                </h2>
+            </div>
+            <div>
+                <h2 className="pokemonStats--stats">
+                    <span>Defense:</span> {props.stats[2].base_stat}
+                </h2>
+            </div>
+            <div>
+                <h2 className="pokemonStats--stats">
+                    <span>Sp. Atk:</span> {props.stats[3].base_stat}
+                </h2>
+            </div>
+            <div>
+                <h2 className="pokemonStats--stats">
+                    <span>Sp. Def:</span> {props.stats[4].base_stat}
+                </h2>
+            </div>
+            <div>
+                <h2 className="pokemonStats--stats">
+                    <span>Speed:</span> {props.stats[5].base_stat}
+                </h2>
+            </div>    
         </div>
     )
 }
