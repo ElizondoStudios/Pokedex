770b6175e524b31d3e5e6cd9e5cbb1202a771e05
Jose Luis <joseluis.elizondof11@outlook.com>
2022-08-28 13:04:12 -0500

    modified some class names

--- components/PokemonStats.jsx
+++ components/PokemonStats.jsx
@@ -2,33 +2,33 @@ function PokemonStats(props){
 
     return(
         <div className="pokemonStats">
-            <div>
-                <h2 className="pokemonStats--stats">
+            <div className="pokemonStats--stat-div">
+                <h2 className="pokemonStats--stat-div--stats">
                     <span>HP:</span> {props.stats[0].base_stat}
                 </h2>
             </div>
-            <div>
-                <h2 className="pokemonStats--stats">
+            <div className="pokemonStats--stat-div">
+                <h2 className="pokemonStats--stat-div--stats">
                     <span>Attack:</span> {props.stats[1].base_stat}
                 </h2>
             </div>
-            <div>
-                <h2 className="pokemonStats--stats">
+            <div className="pokemonStats--stat-div">
+                <h2 className="pokemonStats--stat-div--stats">
                     <span>Defense:</span> {props.stats[2].base_stat}
                 </h2>
             </div>
-            <div>
-                <h2 className="pokemonStats--stats">
+            <div className="pokemonStats--stat-div">
+                <h2 className="pokemonStats--stat-div--stats">
                     <span>Sp. Atk:</span> {props.stats[3].base_stat}
                 </h2>
             </div>
-            <div>
+            <div className="pokemonStats--stat-div">
                 <h2 className="pokemonStats--stats">
                     <span>Sp. Def:</span> {props.stats[4].base_stat}
                 </h2>
             </div>
-            <div>
-                <h2 className="pokemonStats--stats">
+            <div className="pokemonStats--stat-div">
+                <h2 className="pokemonStats--stat-div--stats">
                     <span>Speed:</span> {props.stats[5].base_stat}
                 </h2>
             </div>    
